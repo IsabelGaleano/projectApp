defbbdffef61293d178fba21ed8fa885c5189608
David <drodriguezc1@ucenfotec.ac.cr>
2022-07-13 12:01:59 -0600

    Added registro usuario final component

---
+++ src/main/webapp/app/registro/registro-usuario-final/registro-usuario-final.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-registro-usuario-final',
+  templateUrl: './registro-usuario-final.component.html',
+  styleUrls: ['./registro-usuario-final.component.scss'],
+})
+export class RegistroUsuarioFinalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
