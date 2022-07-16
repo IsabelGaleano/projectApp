df1405b0dac0176b5a68733aff6aeced82f14fe8
David <drodriguezc1@ucenfotec.ac.cr>
2022-07-15 19:25:21 -0600

    Fixing register component

--- src/main/webapp/app/account/register/register.component.ts
+++ src/main/webapp/app/account/register/register.component.ts
@@ -12,8 +12,8 @@ import { RegisterService } from './register.service';
   templateUrl: './register.component.html',
 })
 export class RegisterComponent implements AfterViewInit {
-  @ViewChild('login', { static: false })
-  login?: ElementRef;
+  // @ViewChild('login', { static: false })
+  // login?: ElementRef;
 
   doNotMatch = false;
   error = false;
@@ -22,15 +22,6 @@ export class RegisterComponent implements AfterViewInit {
   success = false;
 
   registerForm = this.fb.group({
-    login: [
-      '',
-      [
-        Validators.required,
-        Validators.minLength(1),
-        Validators.maxLength(50),
-        Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
-      ],
-    ],
     email: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(254), Validators.email]],
     password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
     confirmPassword: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
@@ -44,9 +35,7 @@ export class RegisterComponent implements AfterViewInit {
   ) {}
 
   ngAfterViewInit(): void {
-    if (this.login) {
-      this.login.nativeElement.focus();
-    }
+    console.warn(' ');
   }
 
   register(): void {
@@ -59,11 +48,10 @@ export class RegisterComponent implements AfterViewInit {
     if (password !== this.registerForm.get(['confirmPassword'])!.value) {
       this.doNotMatch = true;
     } else {
-      const login = this.registerForm.get(['login'])!.value;
       const email = this.registerForm.get(['email'])!.value;
       sessionStorage.setItem('usuarioFinalPendiente', email);
       this.registerService
-        .save({ login, email, password, langKey: this.translateService.currentLang })
+        .save({ email, password, langKey: this.translateService.currentLang })
         .subscribe({ next: () => (this.success = true), error: response => this.processError(response) });
       this.router.navigate(['account/verificacion-codigo-usuario-final']);
     }
