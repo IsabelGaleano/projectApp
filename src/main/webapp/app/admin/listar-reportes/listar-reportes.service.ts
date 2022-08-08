b1fb03390907a146ef12abff656708dc269dc476
David <drodriguezc1@ucenfotec.ac.cr>
2022-08-08 12:11:35 -0600

    Added listado de reporte

---
+++ src/main/webapp/app/admin/listar-reportes/listar-reportes.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { ApplicationConfigService } from 'app/core/config/application-config.service';
+
+@Injectable({ providedIn: 'root' })
+export class ListarReportesAdminService {
+  //rootURL = '/api';
+
+  constructor(private http: HttpClient, private applicationConfigService: ApplicationConfigService) {}
+
+  getUsuariosByCorreoElectronico(correoElectronico: string): Observable<any> {
+    let ruta = '/api/usuariosCorreoElectronico/';
+    ruta = ruta.concat(correoElectronico);
+    return this.http.get(ruta);
+  }
+  getMovimientosByIdMonedero(id: number): Observable<any> {
+    let ruta = '/api/movimientosByIdUsuario/';
+    ruta = ruta.concat(id.toString());
+    return this.http.get(ruta);
+  }
+}
